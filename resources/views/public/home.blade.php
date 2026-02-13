7381362413c262032c5aaec591c60c585c26f342
seren <seren.coban@student.hepl.be>
2026-02-13 11:19:21 +0100

    feat : footer added

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -143,22 +143,20 @@
 </main>
 
 <footer class="footer">
-    <div class="footer__container">
         <div class="footer__brand">
             <h2 class="footer__title">Les Pattes Heureuses</h2>
             <p class="footer__text">Rue du Quelquechose 18<br>4500 Ville</p>
             <p class="footer__text">+32 471 32 63 08</p>
-            <p class="footer__text">Du lundi au samedi de 10h à 18h</p>
+            <p class="footer__text">Ouvert du lundi au samedi de <time class="footer__time" datetime="10">10h</time> à <time class="footer__time"  datetime="18">18h</time></p>
         </div>
         <nav class="footer__nav">
-            <h3 class="footer__nav-title">Navigation</h3>
+            <h3 class="footer__title">Navigation</h3>
             <ul class="footer__menu">
-                <li><a class="footer__link" href="{{ __('/') }}">Accueil</a></li>
-                <li><a class="footer__link" href="{{ __('/animals') }}">Nos animaux</a></li>
-                <li><a class="footer__link" href="{{ __('/contact') }}">Nous contacter</a></li>
+                <li class="footer__item"><a class="footer__link" href="{{ __('/') }}">Accueil</a></li>
+                <li class="footer__item"><a class="footer__link" href="{{ __('/animals') }}">Nos animaux</a></li>
+                <li class="footer__item"><a class="footer__link" href="{{ __('/contact') }}">Nous contacter</a></li>
             </ul>
         </nav>
-    </div>
 </footer>
 
 </body>
