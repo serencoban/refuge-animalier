44727143d87cde6f635c94d248629c2726ddf0df
seren <seren.coban@student.hepl.be>
2026-03-09 09:44:07 +0100

    fix : padding adjusted

--- resources/views/components/public/header.blade.php
+++ resources/views/components/public/header.blade.php
@@ -19,7 +19,6 @@
         <ul class="header__navbar__menu">
             <li class="header__navbar__menu-item"><a class="header__link" href="{{ __('/') }}">Accueil</a></li>
             <li class="header__navbar__menu-item"><a class="header__link" href="{{ __('/animals.index') }}">Nos animaux</a></li>
-            <li class="header__navbar__menu-item"><a class="header__link" href="{{ __('/animals.index') }}">Devenir bénévole</a></li>
             <li class="header__navbar__menu-item"><a class="header__cta__btn" href="{{ __('/contact') }}">Nous contacter</a></li>
         </ul>
         <div id="menuToggle">
@@ -34,7 +33,6 @@
                 <ul class="menu">
                     <li class="menu-item"><a href="{{ __('/') }}">Accueil</a></li>
                     <li class="menu-item"><a href="{{ __('/animals.index') }}">Nos animaux</a></li>
-                    <li class="menu-item"><a href="{{ __('/animals.index') }}">Devenir bénévole</a></li>
                     <li class="menu-item"><a href="{{ __('/contact') }}">Nous contacter</a></li>
                 </ul>
             </div>
